2fd6aa9d01c0d4299be3d19ec1eb2d9c772f77df
Thabiso Molise <thabiso.molise@investec.co.za>
2019-10-23 00:05:56 +0200

    Apply look and feel

---
+++ HospitalManagementSystemNet/HospitalManagementSystem.Models/Web/Login.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HospitalManagementSystem.Models.Web
+{
+    public class LoginCredentials
+    {
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ HospitalManagementSystemNet/HospitalManagementSystem.Web/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using HospitalManagementSystem.Models.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HospitalManagementSystem.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View(new LoginCredentials());
+        }
+        // GET: Account
+        [HttpPost]
+        public ActionResult Login(LoginCredentials login)
+        {
+            return View();
+        }
+
+        // GET: Account
+        public ActionResult LoggedIn()
+        {
+            return View();
+        }
+    }
+}
